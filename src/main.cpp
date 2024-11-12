0add3bdb3c0ee74fa6ae953d5de62bf18582dac4
keiseiisobe <keisei.isobe@gmail.com>
2024-11-12 17:18:46 +0900

    fixed main.cpp HttpConfCtx

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 extern Wbsv::Event* ev;
 
+using namespace Wbsv;
+
 /* ---- substitution for test ---- */
 
 // http {
@@ -43,127 +45,127 @@ extern Wbsv::Event* ev;
 // 	}
 // }
 
-void Wbsv::confParse(Wbsv::Webserv& ws)
-{
-	std::vector<ConfCtx*>* confCtxs = new std::vector<ConfCtx*>;
-	HttpConfCtx* httpConfCtx = new HttpConfCtx;
-	ServerCtx sc;
-	sc.addListen("172.17.0.2", "80");
-	VServerCtx vsc;
-	vsc.defaultServer = true;
-	vsc.serverName = "localhost";
-	vsc.clientMaxBodySize = 1000000;
-	vsc.errorPages["404"] = "/404.html";
-	// this must be dynamicaly allocated
-	HtmlLocationCtx* hlc = new HtmlLocationCtx;
-	hlc->path = "/";
-	hlc->root = "/root/webserv/test/html";
-	hlc->index = "index.html";
-	hlc->autoindex = true;
-	vsc.addLocationCtx((LocationCtx*)hlc);
-	// this must be dynamicaly allocated
-	HtmlLocationCtx* hlc2 = new HtmlLocationCtx;
-	hlc2->path = "/images/";
-	hlc2->root = "/root/webserv/test";
-	vsc.addLocationCtx((LocationCtx*)hlc2);
-	// this must be dynamicaly allocated
-	ReturnLocationCtx* rlc = new ReturnLocationCtx;
-	rlc->path = "/redirect";
-	rlc->redirect.first = "301";
-	rlc->redirect.second = "http://nginx.org";
-	vsc.addLocationCtx((LocationCtx*)rlc);
-	// this must be dynamicaly allocated
-	CgiLocationCtx* clc = new CgiLocationCtx;
-	clc->path = "/php/";
-	clc->index = "upload.php";
-	clc->param["SCRIPT_FILENAME"] = "/root/webserv/test/cgi$cgi_path_info";
-	clc->store = "/root/webserv/test/images/";
-	vsc.addLocationCtx((LocationCtx*)clc);
-	HtmlLocationCtx* hlc3 = new HtmlLocationCtx;
-	hlc3->path = "/404.html";
-	hlc3->root = "/root/webserv/test/html";
-	vsc.addLocationCtx((LocationCtx*)hlc3);
-	sc.addVServerCtx(vsc);
-	httpConfCtx->addServerCtx(sc);
-	confCtxs->push_back(httpConfCtx);
-	ws.setConfCtxs(confCtxs);
-	std::vector<LocationCtx*>* v = vsc.getLocationCtxs();
-}
+// void Wbsv::confParse(Wbsv::Webserv& ws)
+// {
+// 	std::vector<ConfCtx*>* confCtxs = new std::vector<ConfCtx*>;
+// 	HttpConfCtx* httpConfCtx = new HttpConfCtx;
+// 	ServerCtx sc;
+// 	sc.addListen("172.17.0.2", "80");
+// 	VServerCtx vsc;
+// 	vsc.defaultServer = true;
+// 	vsc.serverName = "localhost";
+// 	vsc.clientMaxBodySize = 1000000;
+// 	vsc.errorPages["404"] = "/404.html";
+// 	// this must be dynamicaly allocated
+// 	HtmlLocationCtx* hlc = new HtmlLocationCtx;
+// 	hlc->path = "/";
+// 	hlc->root = "/root/webserv/test/html";
+// 	hlc->index = "index.html";
+// 	hlc->autoindex = true;
+// 	vsc.addLocationCtx((LocationCtx*)hlc);
+// 	// this must be dynamicaly allocated
+// 	HtmlLocationCtx* hlc2 = new HtmlLocationCtx;
+// 	hlc2->path = "/images/";
+// 	hlc2->root = "/root/webserv/test";
+// 	vsc.addLocationCtx((LocationCtx*)hlc2);
+// 	// this must be dynamicaly allocated
+// 	ReturnLocationCtx* rlc = new ReturnLocationCtx;
+// 	rlc->path = "/redirect";
+// 	rlc->redirect.first = "301";
+// 	rlc->redirect.second = "http://nginx.org";
+// 	vsc.addLocationCtx((LocationCtx*)rlc);
+// 	// this must be dynamicaly allocated
+// 	CgiLocationCtx* clc = new CgiLocationCtx;
+// 	clc->path = "/php/";
+// 	clc->index = "upload.php";
+// 	clc->param["SCRIPT_FILENAME"] = "/root/webserv/test/cgi$cgi_path_info";
+// 	clc->store = "/root/webserv/test/images/";
+// 	vsc.addLocationCtx((LocationCtx*)clc);
+// 	HtmlLocationCtx* hlc3 = new HtmlLocationCtx;
+// 	hlc3->path = "/404.html";
+// 	hlc3->root = "/root/webserv/test/html";
+// 	vsc.addLocationCtx((LocationCtx*)hlc3);
+// 	sc.addVServerCtx(vsc);
+// 	httpConfCtx->addServerCtx(sc);
+// 	confCtxs->push_back(httpConfCtx);
+// 	ws.setConfCtxs(confCtxs);
+// 	std::vector<LocationCtx*>* v = vsc.getLocationCtxs();
+// }
 
-void printConf(std::vector<Wbsv::ConfCtx*>* confCtxs)
-{
-	printLog(LOG_DEBUG, "configuration file parsed");
-	std::cout << "\n\n---------------------------------------------\n";
-	using namespace Wbsv;
-	std::cout << "confCtxs (size: " << confCtxs->size() << "):" << std::endl;
-	std::vector<ConfCtx*>::iterator it;
-	for (it = (*confCtxs).begin(); it != (*confCtxs).end(); it++)
-	{
-		HttpConfCtx* hcc = (HttpConfCtx*)*it;
-		std::cout << "\tmainConf (size: " << hcc->getMainCtxs().size() << "):" << std::endl;
-		std::cout << "\tserverConf (size: " << hcc->getServerCtxs().size() << "):" << std::endl;
-		std::vector<ServerCtx>::iterator sit;
-		for (sit = hcc->getServerCtxs().begin(); sit != hcc->getServerCtxs().end(); sit++)
-		{
-			std::cout << std::setw(20) << std::left
-					  << "\t\tlisten.first: " << sit->getListen().first << std::endl;
-			std::cout << std::setw(20) << std::left
-					  << "\t\tlisten.second: " << sit->getListen().second << std::endl;
-			std::vector<VServerCtx>::iterator vsit;
-			for (vsit = sit->getVServerCtxs()->begin(); vsit != sit->getVServerCtxs()->end();
-				 vsit++)
-			{
-				std::cout << std::setw(20) << std::left << "\n\t\tvserverConf:\n";
-				std::cout << std::setw(20) << std::left << "\t\tdefaultServer: " << std::boolalpha
-						  << vsit->defaultServer << std::endl;
-				std::cout << std::setw(20) << std::left << "\t\tserver_name: " << vsit->serverName
-						  << std::endl;
-				std::cout << std::setw(20) << std::left
-						  << "\t\tlocationConf (size: " << vsit->getLocationCtxs()->size()
-						  << "):" << std::endl;
-				std::vector<LocationCtx*>::const_iterator lit;
-				for (lit = vsit->getLocationCtxs()->begin(); lit != vsit->getLocationCtxs()->end();
-					 lit++)
-				{
-					HtmlLocationCtx* hlc;
-					CgiLocationCtx* clc;
-					ReturnLocationCtx* rlc;
-					// std::cout.width(20);
-					if ((hlc = dynamic_cast<HtmlLocationCtx*>(*lit)))
-					{
-						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << hlc->path
-								  << std::endl;
-						std::cout << std::setw(20) << std::left << "\t\t\troot: " << hlc->root
-								  << std::endl;
-						std::cout << std::setw(20) << std::left << "\t\t\tindex: " << hlc->index
-								  << std::endl;
-					}
-					else if ((clc = dynamic_cast<CgiLocationCtx*>(*lit)))
-					{
-						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << clc->path
-								  << std::endl;
-						std::cout << std::setw(20) << std::left
-								  << "\t\t\tSCRIPT_FILENAME: " << clc->param["SCRIPT_FILENAME"]
-								  << std::endl;
-						std::cout << std::setw(20) << std::left << "\t\t\tindex: " << clc->index
-								  << std::endl;
-					}
-					else if ((rlc = dynamic_cast<ReturnLocationCtx*>(*lit)))
-					{
-						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << rlc->path
-								  << std::endl;
-						std::cout << std::setw(20) << std::left
-								  << "\t\t\tredirect: " << rlc->redirect.first << " "
-								  << rlc->redirect.second << std::endl;
-					}
-					std::cout << std::endl;
-				}
-				std::cout << std::endl;
-			}
-		}
-	}
-	std::cout << "\n---------------------------------------------\n\n";
-}
+// void printConf(std::vector<Wbsv::ConfCtx*>* confCtxs)
+// {
+// 	printLog(LOG_DEBUG, "configuration file parsed");
+// 	std::cout << "\n\n---------------------------------------------\n";
+// 	using namespace Wbsv;
+// 	std::cout << "confCtxs (size: " << confCtxs->size() << "):" << std::endl;
+// 	std::vector<ConfCtx*>::iterator it;
+// 	for (it = (*confCtxs).begin(); it != (*confCtxs).end(); it++)
+// 	{
+// 		HttpConfCtx* hcc = (HttpConfCtx*)*it;
+// 		std::cout << "\tmainConf (size: " << hcc->getMainCtxs().size() << "):" << std::endl;
+// 		std::cout << "\tserverConf (size: " << hcc->getServerCtxs().size() << "):" << std::endl;
+// 		std::vector<ServerCtx>::iterator sit;
+// 		for (sit = hcc->getServerCtxs().begin(); sit != hcc->getServerCtxs().end(); sit++)
+// 		{
+// 			std::cout << std::setw(20) << std::left
+// 					  << "\t\tlisten.first: " << sit->getListen().first << std::endl;
+// 			std::cout << std::setw(20) << std::left
+// 					  << "\t\tlisten.second: " << sit->getListen().second << std::endl;
+// 			std::vector<VServerCtx>::iterator vsit;
+// 			for (vsit = sit->getVServerCtxs()->begin(); vsit != sit->getVServerCtxs()->end();
+// 				 vsit++)
+// 			{
+// 				std::cout << std::setw(20) << std::left << "\n\t\tvserverConf:\n";
+// 				std::cout << std::setw(20) << std::left << "\t\tdefaultServer: " << std::boolalpha
+// 						  << vsit->defaultServer << std::endl;
+// 				std::cout << std::setw(20) << std::left << "\t\tserver_name: " << vsit->serverName
+// 						  << std::endl;
+// 				std::cout << std::setw(20) << std::left
+// 						  << "\t\tlocationConf (size: " << vsit->getLocationCtxs()->size()
+// 						  << "):" << std::endl;
+// 				std::vector<LocationCtx*>::const_iterator lit;
+// 				for (lit = vsit->getLocationCtxs()->begin(); lit != vsit->getLocationCtxs()->end();
+// 					 lit++)
+// 				{
+// 					HtmlLocationCtx* hlc;
+// 					CgiLocationCtx* clc;
+// 					ReturnLocationCtx* rlc;
+// 					// std::cout.width(20);
+// 					if ((hlc = dynamic_cast<HtmlLocationCtx*>(*lit)))
+// 					{
+// 						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << hlc->path
+// 								  << std::endl;
+// 						std::cout << std::setw(20) << std::left << "\t\t\troot: " << hlc->root
+// 								  << std::endl;
+// 						std::cout << std::setw(20) << std::left << "\t\t\tindex: " << hlc->index
+// 								  << std::endl;
+// 					}
+// 					else if ((clc = dynamic_cast<CgiLocationCtx*>(*lit)))
+// 					{
+// 						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << clc->path
+// 								  << std::endl;
+// 						std::cout << std::setw(20) << std::left
+// 								  << "\t\t\tSCRIPT_FILENAME: " << clc->param["SCRIPT_FILENAME"]
+// 								  << std::endl;
+// 						std::cout << std::setw(20) << std::left << "\t\t\tindex: " << clc->index
+// 								  << std::endl;
+// 					}
+// 					else if ((rlc = dynamic_cast<ReturnLocationCtx*>(*lit)))
+// 					{
+// 						std::cout << std::setw(20) << std::left << "\t\t\tpath: " << rlc->path
+// 								  << std::endl;
+// 						std::cout << std::setw(20) << std::left
+// 								  << "\t\t\tredirect: " << rlc->redirect.first << " "
+// 								  << rlc->redirect.second << std::endl;
+// 					}
+// 					std::cout << std::endl;
+// 				}
+// 				std::cout << std::endl;
+// 			}
+// 		}
+// 	}
+// 	std::cout << "\n---------------------------------------------\n\n";
+// }
 
 void printListening(std::vector<Wbsv::Listening>* lss)
 {
@@ -201,7 +203,7 @@ void printListening(std::vector<Wbsv::Listening>* lss)
 	std::cout << "\n---------------------------------------------\n\n";
 }
 
-void printHttpConfCtx(HttpConfCtx httpCtx)
+void printHttpConfCtx(HttpConfCtx& httpCtx)
 {
 	std::cout << "--- HttpConfCtx ---" << std::endl << std::endl;
 	for (std::vector<ServerCtx>::iterator it = httpCtx.getServerCtxs().begin();
@@ -277,7 +279,7 @@ void printHttpConfCtx(HttpConfCtx httpCtx)
 					std::cout << "path: " << retur->getPath() << std::endl;
 					std::cout << "autoIndex: " << retur->isAutoIndex() << std::endl;
 					std::cout << "LimitExcept: " << retur->getLimitExcept() << std::endl;
-					std::vector<std::string> re = retur->getReturn();
+					std::vector<std::string> re = retur->getRedirect();
 					std::cout << "return: ";
 					for (std::vector<std::string>::iterator it = re.begin(); it != re.end(); it++)
 						std::cout << *it << " ";
@@ -311,14 +313,14 @@ int main(int argc, char* argv[])
 	{
 		if (argc != 2)
 			return 0;
-		// Wbsv::Webserv ws;
+		Wbsv::Webserv ws;
 		/* confFileModule */
-		// confParse(ws);
 		std::vector<ConfCtx*> ctx = ConfParse::confParse(confFileName);
 		if (!ctx.empty())
 			httpCtx = dynamic_cast<HttpConfCtx*>(ctx.back());
 		if (httpCtx != NULL)
 			printHttpConfCtx(*httpCtx);
+		ws.setConfCtxs(&ctx);
 		// printConf(ws.getConfCtxs());
 		/* initialize 'ws' elements with values got by configuration file */
 		/* ex. */
